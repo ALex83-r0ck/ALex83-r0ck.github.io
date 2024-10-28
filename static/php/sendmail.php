897b0817fc74c9203151b21db06f2501e88d5e50
ALex83-r0ck <145429827+ALex83-r0ck@users.noreply.github.com>
2024-10-28 21:31:48 +0100

    #

---
+++ static/php/sendmail.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+    $to = "deine.email@beispiel.com";
+    $subject = "Neue Nachricht von $name";
+    $body = "Name: $name\n";
+    $body .= "E-Mail: $email\n";
+    $body .= "Nachricht: $message\n";
+    
+    if (mail($to, $subject, $body)) {
+        echo "Nachricht erfolgreich gesendet!";
+    } else {
+        echo "Fehler beim Senden der Nachricht.";
+    }
+}
+?>
