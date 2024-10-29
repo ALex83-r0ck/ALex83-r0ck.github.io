63ff2f3b4e617e76a69970ef3aaeca6c04050cfa
ALex83-r0ck <145429827+ALex83-r0ck@users.noreply.github.com>
2024-10-29 13:15:27 +0100

    #

--- scripts.js
+++ scripts.js
@@ -14,28 +14,29 @@ document.querySelectorAll('.toggle-details').forEach(button => {
     });
 });
 
-// Overlay-Management
-document.getElementById('image-overlay').addEventListener('click', function() {
-    const overlayImage = document.getElementById('overlay-image');
-    const profileImage = document.getElementById('profile-image');
-    const profileDescription = document.getElementById('profile-description');
-    const overlay = document.getElementById('image-overlay');
+document.addEventListener('DOMContentLoaded', function() {
+    // Overlay-Management
+    document.getElementById('image-overlay').addEventListener('click', function() {
+        const overlayImage = document.getElementById('overlay-image');
+        const profileImage = document.getElementById('profile-image');
+        const profileDescription = document.getElementById('profile-description');
+        const overlay = document.getElementById('image-overlay');
+        overlay.style.display = 'none';
+        overlayImage.style.display = 'none';
+        profileImage.style.display = 'block';
+        profileDescription.style.display = 'block';
+    });
 
-    overlay.style.display = 'none'; 
-    overlayImage.style.display = 'none'; 
-    profileImage.style.display = 'block'; 
-    profileDescription.style.display = 'block'; 
+    // Profilbild klickbar machen, um Overlay und Text auszublenden
+    document.getElementById('profile-image').addEventListener('click', function() {
+        const overlayImage = document.getElementById('overlay-image');
+        const profileImage = document.getElementById('profile-image');
+        const profileDescription = document.getElementById('profile-description');
+        const overlay = document.getElementById('image-overlay');
+        overlay.style.display = 'flex';
+        overlayImage.style.display = 'block';
+        profileImage.style.display = 'none';
+        profileDescription.style.display = 'none';
+    });
 });
 
-// Profilbild klickbar machen, um Overlay und Text auszublenden
-document.getElementById('profile-image').addEventListener('click', function() {
-    const overlayImage = document.getElementById('overlay-image');
-    const profileImage = document.getElementById('profile-image');
-    const profileDescription = document.getElementById('profile-description');
-    const overlay = document.getElementById('image-overlay');
-
-    overlay.style.display = 'flex';
-    overlayImage.style.display = 'block';
-    profileImage.style.display = 'none';
-    profileDescription.style.display = 'none';
-});
