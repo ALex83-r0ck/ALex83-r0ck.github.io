ad515082180c8da61da764132efd7a30e865f06f
ALex83-r0ck <145429827+ALex83-r0ck@users.noreply.github.com>
2024-10-29 13:06:24 +0100

    #

--- scripts.js
+++ scripts.js
@@ -1,37 +1,3 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    // Prevent default behavior (this will be handled later)
-    // event.preventDefault(); 
-
-    const name = document.getElementById('name').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const message = document.getElementById('message').value.trim();
-
-    // Eingabevalidierung
-    if (!name || !email || !message) {
-        alert('Bitte fülle Sie alle Felder aus.');
-        return;
-    }
-
-    if (!validateEmail(email)) {
-        alert('Bitte geben Sie eine gültige E-Mail-Adresse ein.');
-        return;
-    }
-
-    // Um das Formular zu senden
-    this.submit(); // Dies sendet das Formular
-
-    alert(`Danke für Ihre Nachricht, ${name}! Ich werde mich bald bei Ihnen melden.`);
-
-    // Formular zurücksetzen
-    this.reset();
-});
-
-// E-Mail-Validierungsfunktion
-function validateEmail(email) {
-    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(email);
-}
-
 // Scroll-Animationen
 document.querySelectorAll('.toggle-details').forEach(button => {
     button.addEventListener('click', () => {
